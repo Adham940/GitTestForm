3bb3ae578f534fdd3ed50c2247c12afd4900c02f
Adham <adhmwaled94@gmail.com>
2025-04-24 19:31:03 +0200

    Design

--- GitTestForm/Form2.Designer.cs
+++ GitTestForm/Form2.Designer.cs
@@ -35,7 +35,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(191, 36);
+            label1.Location = new Point(157, 74);
             label1.Name = "label1";
             label1.Size = new Size(49, 17);
             label1.TabIndex = 1;
